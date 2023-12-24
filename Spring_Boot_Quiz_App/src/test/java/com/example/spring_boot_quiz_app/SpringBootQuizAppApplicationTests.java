82604239563ff54a82fe48c21d137930be73c8ae
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2023-12-24 22:18:01 +0600

    start project

---
+++ Spring_Boot_Quiz_App/src/test/java/com/example/spring_boot_quiz_app/SpringBootQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_quiz_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
